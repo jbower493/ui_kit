e9f36285cc98a9521f853a44235ee02a0d24699c
Jamie Bower <j_bower@hotmail.co.uk>
2023-02-19 17:42:43 +1100

    refactor: export both properly from button folder

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,2 +1,2 @@
-// export { default as StyledButton } from './Button.styled'
-// export { default as UnstyledButton } from './Button.unstyled'
+export { default as UnstyledButton } from './Button.unstyled'
+export { default as StyledButton } from './Button.styled'
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,2 @@
 export { default as Anchor } from './Anchor'
-export { default as UnstyledButton } from './Button/Button.unstyled'
-export { default as StyledButton } from './Button/Button.styled'
+export * from './Button'
